54e9bbca0cba8ea5e31c7b7c682bd2763e34d385
Stesh_ <45111570+MouStesh@users.noreply.github.com>
2022-05-21 03:02:12 +0200

    Added Zorium ingots/raw/nugget and textures

---
+++ src/main/java/net/princess/citadelmod/item/ModItems.java
@@ -0,0 +1,31 @@
+package net.princess.citadelmod.item;
+
+import net.minecraft.world.item.CreativeModeTab;
+import net.minecraft.world.item.Item;
+import net.minecraftforge.eventbus.api.IEventBus;
+import net.minecraftforge.registries.DeferredRegister;
+import net.minecraftforge.registries.ForgeRegistries;
+import net.minecraftforge.registries.RegistryObject;
+import net.princess.citadelmod.CitadelMod;
+
+public class ModItems {
+    public static final DeferredRegister<Item> ITEMS =
+            DeferredRegister.create(ForgeRegistries.ITEMS, CitadelMod.MOD_ID);
+
+
+    public static final RegistryObject<Item> ZORIUM_INGOT = ITEMS.register("zorium_ingot",
+            () -> new Item(new Item.Properties().tab(CreativeModeTab.TAB_MISC)));
+
+    public static final RegistryObject<Item> ZORIUM_NUGGET = ITEMS.register("zorium_nugget",
+            () -> new Item(new Item.Properties().tab(CreativeModeTab.TAB_MISC)));
+
+    public static final RegistryObject<Item> RAW_ZORIUM = ITEMS.register("raw_zorium",
+            () -> new Item(new Item.Properties().tab(CreativeModeTab.TAB_MISC)));
+
+
+    public static void register(IEventBus eventBus){
+        ITEMS.register(eventBus);
+    }
+
+
+}
